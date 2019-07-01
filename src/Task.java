f9501ab5a68d09d7f07fbadc5f6fdc1a3d85da77
Jonathan Nieblas <jniebla1@msudenver.edu>
2019-07-01 17:12:33 -0600

    Implementing the basic design for Task class.

---
+++ src/Task.java
@@ -0,0 +1,59 @@
+/*
+Task:
+    - Class for making task instance variables.
+    -
+
+Vars:
+    - id: the task's unique ID
+    - name: User-given task name
+    - description: User-given task description
+    - time: Length of task
+**/
+public class Task {
+  private int id;
+  private String name;
+  private String description;
+  private double time;
+  private double startTime;
+  private double endTime;
+
+  public Task() {
+  }
+
+  public void setName(String name){
+    this.name = name;
+  }
+
+  public void setDescription(String description){
+    this.description = description;
+  }
+
+  public void setTime(int time){
+    this.time = time;
+  }
+
+  public void setStartTime(int start){
+    this.startTime = start;
+  }
+
+  public void setEndTime(){
+    this.endTime = this.startTime + this.endTime;
+  }
+
+  public int getid(){
+    return this.id;
+  }
+
+  public String getName(){
+    return this.name;
+  }
+
+  public String getDescription(){
+    return this.description;
+  }
+
+  public String getTime(){
+    return
+  }
+
+}
